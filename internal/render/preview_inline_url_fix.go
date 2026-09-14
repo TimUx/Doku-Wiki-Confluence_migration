a58d7f729b46857d1daf1be1dd866bade2d8e699
Timo <email@timobraun.de>
2026-09-14 15:13:17 +0200

    fix(render): prevent italic matcher crossing HTML attributes

--- internal/render/preview_inline_url_fix.go
+++ internal/render/preview_inline_url_fix.go
@@ -4,5 +4,5 @@ import "regexp"
 
 func init() {
 	// Keep the DokuWiki italic matcher from crossing generated HTML tags.
-	italic = regexp.MustCompile(`//([^/<\n]+?)//`)
+	italic = regexp.MustCompile(`//([^/<>"]+?)//`)
 }
