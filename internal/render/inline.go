4e0c1839fe6924940a3d0b7f476da3ef839b4253
Timo <email@timobraun.de>
2026-09-14 18:26:03 +0200

    fix(render): use safe no-format placeholder

--- internal/render/inline.go
+++ internal/render/inline.go
@@ -24,7 +24,7 @@ var wrapWidth = regexp.MustCompile(`^[0-9]+(?:\.[0-9]+)?(?:%|px|em|rem|vw|vh)$`)
 var lineBreak = regexp.MustCompile(`\\\\(?:\s+|$)`)
 var noFormat = regexp.MustCompile(`(?s)%%(.*?)%%`)
 
-const noFormatToken = "\x00DOKUWIKI_NOFORMAT_%d\x00"
+const noFormatToken = "\x00DOKUWIKINOFMT%d\x00"
 
 type noFormatSpan struct {
 	token string
